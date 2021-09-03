47c3ac970d3e1bf3d75f6ce60759dfd187fe4f76
syotaKUSHIZAKI <syota.kushizaki@gmail.com>
2021-09-03 17:02:53 +0900

    基本機能完成

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import logo from "./logo.svg";
+import React from 'react';
 import "./App.css";
 import { buildTable } from "./components/BuildTable";
 import { basicskill } from "./data/WEAPONSKILL";
 import { CALCULATEINTERFACE } from "./data/WEAPONSKILL";
 import { weapons } from "./data/WEAPONS";
 
-function computeWeaponSkill(/*装備中の武器が引数になる*/) {
-  let obj_equiped_weapon = {
-    1: weapons["Ixaba"],
-    2: weapons["Ixaba"],
-  };
+function computeWeaponSkill(list) {
+  let obj_equiped_weapon = {}
+  let CNT = 1
+  list.forEach((value) => {
+    obj_equiped_weapon[CNT] = weapons[value];
+    CNT += 1;
+  })
 
   //DeepCopyMethod -> JSON.parse(JSON.stringify(***DeepCopyTarget***))
   let obj_output = JSON.parse(JSON.stringify(CALCULATEINTERFACE));
@@ -25,14 +28,18 @@ function computeWeaponSkill(/*装備中の武器が引数になる*/) {
     });
   });
   return (
-    <>
+    <ul style={{ listStyle: "circle", textAlign: "left", fontSize: "14px" }}>
       {Object.keys(obj_output).map((key) => (
-        <p>
+        <li>
           {key}: {obj_output[key]}
-        </p>
+        </li>
       ))}
-    </>
+    </ul>
   );
+}
+
+function renderEquiped(list){
+  return list.map((value) => <p>{value}</p>)
 }
 
 function App() {
@@ -40,19 +47,37 @@ function App() {
     display: "flex",
   };
   const style_app_left = {
-    backgroundColor: "#505050",
+    width: "250px",
+    magin: "15px",
+    padding: "20px 0px",
+    backgroundColor: "#aaaaaa",
   };
 
   const style_app_right = {
     width: "50%",
   };
 
+  const [list_equiped, addEquiped] = React.useState([]);
+
+  React.useEffect(()=>{
+
+  })
+
+  function addEquipe(e){
+    let item = JSON.parse(JSON.stringify(list_equiped));
+    item.push(e.target.value);
+    console.log(item)
+    addEquiped(item);
+  }
+
   return (
     <>
+      <div>{list_equiped.map((value) => <input type="button" value={value} />)}</div>
       <div style={style_app} class="App">
+        
         <div style={style_app_left} class="AppLeft">
-          <p>AppLeft</p>
-          {computeWeaponSkill()}
+          <p style={{ backgroundColor: "#ffffff" }}>AppLeft</p>
+          {computeWeaponSkill(list_equiped)}
         </div>
 
         <div style={style_app_right} class="AppRight">
@@ -61,6 +86,7 @@ function App() {
             {Object.keys(weapons).map((key) => (
               <input
                 type="button"
+                onClick={addEquipe}
                 style={{ margin: "1px" }}
                 value={weapons[key].name}
               />
--- src/data/WEAPONS.js
+++ src/data/WEAPONS.js
@@ -1,9 +1,9 @@
 import { basicskill } from "./WEAPONSKILL"
 
 export const weapons = {
-    "Ixaba":{name: "イクサバ",element: "", skill: {1: {type: "通常攻刃", lank: "特大"}, 2: {type: "通常渾身", lank: "中"}}},
-    "ガリレオサイト":{name: "ガリレオサイト"},
-    "虚無乃哭風": {name: "虚無乃哭風"},
-    "エデン": {name: "エデン",},
-    "アーク": {name: "アーク",},
+    "イクサバ":{name: "イクサバ",element: "", skill: {1: {type: "通常攻刃", lank: "特大"}, 2: {type: "通常渾身", lank: "中"}}},
+    "ガリレオサイト":{name: "ガリレオサイト", skill: {1: {type: "通常攻刃", lank: "特大"}, 2: {type: "通常渾身", lank: "中"}}},
+    "虚無乃哭風": {name: "虚無乃哭風", skill: {1: {type: "通常攻刃", lank: "特大"}, 2: {type: "通常渾身", lank: "中"}}},
+    "エデン": {name: "エデン", skill: {1: {type: "通常攻刃", lank: "特大"}, 2: {type: "通常渾身", lank: "中"}}},
+    "アーク": {name: "アーク", skill: {1: {type: "通常攻刃", lank: "特大"}, 2: {type: "通常渾身", lank: "中"}}},
 }

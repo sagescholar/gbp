047fc4b90f416d89c096d75d5bb7a923f95971c8
syotaKUSHIZAKI <syota.kushizaki@gmail.com>
2021-09-22 20:22:16 +0900

    Icon実装

--- src/components/BuildWeaponSearchElementButton.js
+++ src/components/BuildWeaponSearchElementButton.js
@@ -1,4 +1,5 @@
 import cloneDeep from "lodash.clonedeep";
+import { ELEMENT_ICON_INTERFACE } from "../data/ELEMENT"
 
 
 export const buildWeaponSearchElementButton = (parent_state_weapon_search_element,parentSetStateWeaponSearchButton) => {
@@ -9,15 +10,21 @@ export const buildWeaponSearchElementButton = (parent_state_weapon_search_elemen
     }
     
     return(
-        <dvi class="weapon-element-wrapper">
+        <dvi style={{display: "block",backgroundColor: "#666666" ,width: "", padding:"5px"}} class="weapon-element-wrapper">
+            <>
             {Object.keys(parent_state_weapon_search_element).map((element) => {
                 if(parent_state_weapon_search_element[element]){
-                    return <a onClick={()=>onClick(element)} style={{backgroundColor:"white"}}>{element}</a>
+                    return <a onClick={()=>onClick(element)} style={{backgroundColor:""}}>
+                        <img style={{padding: "2px",width: "20px"}}src={process.env.PUBLIC_URL + "/" + ELEMENT_ICON_INTERFACE[element] + ".png"} /> 
+                    </a>
                 }
                 else{
-                    return <a onClick={()=>onClick(element)} style={{backgroundColor:"black"}}>{element}</a>
+                    return <a onClick={()=>onClick(element)} style={{backgroundColor:""}}>
+                        <img style={{padding: "2px",width: "20px",opacity: "0.5"}}src={process.env.PUBLIC_URL + "/" + ELEMENT_ICON_INTERFACE[element] + ".png"} /> 
+                    </a>
                 }
             })}
+            </>
         </dvi>
     )
 }
--- src/data/ELEMENT.js
+++ src/data/ELEMENT.js
@@ -16,6 +16,15 @@ export const ELEMENT_TOGGLE_INTERFACE = {
     "闇": true,
 }
 
+export const ELEMENT_ICON_INTERFACE = {
+    "火": "images/Icon_Element_Fire",
+    "水": "images/Icon_Element_Water",
+    "土": "images/Icon_Element_Earth",
+    "風": "images/Icon_Element_Wind",
+    "光": "images/Icon_Element_Light",
+    "闇": "images/Icon_Element_Dark",
+}
+
 export const ELEMENT_STYLE = {
     火: '#f25252',
     水: "#525CF2",
